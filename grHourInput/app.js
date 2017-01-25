19494a8662755c758e074efb5453185f6d5e36e5
Gabriel Rohden <gabriel_rohden@outlook.com>
2017-01-25 00:08:26 -0200

    Forgot app font on last commit

--- grHourInput/app.js
+++ grHourInput/app.js
@@ -5,8 +5,19 @@
     'use strict';
     /*global angular*/
     angular.module('Demo', ['GRHI'])
-        .controller('demoController',function ($scope) {
+        .controller('demoController',function ($scope, $interval) {
             $scope.milliSeconds = 3600000;
             $scope.hourString = "25h 10m";
+
+            function updateCurrentDayMillis() {
+                console.debug('updating');
+                var date = new Date();
+                $scope.currentDayMillis = (date.getHours()+(date.getMinutes()/60))*60*60*1000;
+            }
+
+            //Make first update happen
+            updateCurrentDayMillis();
+
+            $interval(updateCurrentDayMillis,5000);
         });
 }());
