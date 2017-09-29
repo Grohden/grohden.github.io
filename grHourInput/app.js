0497b7521b64a33f93deb8c0de584b3c815913a1
Gabriel Rohden <gabriel_rohden@outlook.com>
2017-09-28 23:23:04 -0300

    update to latest version of hour input (revert to the expected option), update demo and added strict di.

--- grHourInput/app.js
+++ grHourInput/app.js
@@ -2,22 +2,30 @@
  * Created by gabriel.rohden on 30/11/2016.
  */
 (function () {
-    'use strict';
-    /*global angular*/
-    angular.module('Demo', ['GRHI'])
-        .controller('demoController',function ($scope, $interval) {
-            $scope.milliSeconds = 3600000;
-            $scope.hourString = "25h 10m";
+  'use strict';
+  /*global angular*/
+  angular
+    .module('Demo', ['GRHI'])
+    .controller('demoController', ['$scope', '$interval', demoController]);
+  
+  function demoController($scope, $interval) {
+    $scope.milliSeconds = 3600000;
+    $scope.hourString = "25h 10m";
+    $scope.alertFilterOptions = {
+      expected: 'milliseconds',
+      minString: 'the beginning of the day'
+    }
 
-            function updateCurrentDayMillis() {
-                console.debug('updating');
-                var date = new Date();
-                $scope.currentDayMillis = (date.getHours()+(date.getMinutes()/60))*60*60*1000;
-            }
+    function updateCurrentDayMillis() {
+      console.debug('updating');
+      var date = new Date();
+      $scope.currentDayMillis = (date.getHours() + (date.getMinutes() / 60)) * 60 * 60 * 1000;
+    }
 
-            //Make first update happen
-            updateCurrentDayMillis();
+    //Make first update happen
+    updateCurrentDayMillis();
 
-            $interval(updateCurrentDayMillis,5000);
-        });
+    $interval(updateCurrentDayMillis, 5000);
+  }
+  
 }());
